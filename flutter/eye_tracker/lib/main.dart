c4f173cf774b57ed5904f818dbe467001d1b09ae
kwonyj1022 <kwonyj1022@naver.com>
2025-03-05 05:57:56 +0900

    feat: add eye tracking

--- flutter/eye_tracker/lib/main.dart
+++ flutter/eye_tracker/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:seeso_flutter/event/gaze_info.dart';
+import 'package:seeso_flutter/seeso.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,58 +11,82 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: EyeTrackingScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  final String title;
+class EyeTrackingScreen extends StatefulWidget {
+  const EyeTrackingScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<EyeTrackingScreen> createState() => _EyeTrackingScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _EyeTrackingScreenState extends State<EyeTrackingScreen> {
+  final SeeSo _seeso = SeeSo();
+  double _gazeX = 0;
+  double _gazeY = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _initEyeTracking();
+  }
+
+  Future<void> _initEyeTracking() async {
+    final hasPermission = await _seeso.checkCameraPermission();
+    if (!hasPermission) {
+      await _seeso.requestCameraPermission();
+    }
+
+    const licenseKey = 'lisenseKey'; // TODO: seeso.io에서 발급받은 키
+    await _seeso.initGazeTracker(licenseKey: licenseKey);
+
+    _seeso.startTracking();
+
+    _seeso.getGazeEvent().listen((event) {
+      final gazeInfo = GazeInfo(event);
+      if (gazeInfo.trackingState == TrackingState.SUCCESS) {
+        setState(() {
+
+          _gazeX = gazeInfo.x;
+          _gazeY = gazeInfo.y;
+        });
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Stack(
+        children: [
+          const Center(child: Text('시선 추적 영역')),
+
+          // 시선 위치 표시용 빨간 점
+          Positioned(
+            left: _gazeX - 10,  // 점의 중심 정렬
+            top: _gazeY - 10,
+            child: Container(
+              width: 20,
+              height: 20,
+              decoration: const BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.circle,
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  @override
+  void dispose() {
+    _seeso.stopTracking();
+    super.dispose();
   }
 }
